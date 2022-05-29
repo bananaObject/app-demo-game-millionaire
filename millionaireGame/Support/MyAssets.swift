6595da2e39df7efd038869be0b97ed480239a0d7
Ke4a <helpomos@gmail.com>
2022-05-29 12:20:45 +0500

    first settings

---
+++ millionaireGame/Support/MyAssets.swift
@@ -0,0 +1,21 @@
+//
+//  MyAssets.swift
+//  millionaireGame
+//
+//  Created by Ke4a on 28.05.2022.
+//
+
+import Foundation
+import UIKit
+
+struct MyAssetsImage {
+    static var logo: String {
+        "logo"
+    }
+}
+
+struct MyAssetsColor {
+    static var mainColor: UIColor {
+        UIColor(named: "mainColor") ?? .red
+    }
+}
